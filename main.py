eb7f991ac0ae46092110907658ea16559c6686d7
Divyanshu Dwivedi <divyanshu.d055123@gmail.com>
2026-06-11 12:01:04 +0530

    env add

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
 import logging
 import httpx
 
 app=FastAPI()
 logging.basicConfig(level=logging.INFO)
 
-@app.get("/todo")
-async def get_todo():
-    async with httpx.AsyncClient() as client:
-        response = await client.get("https://jsonplaceholder.typicode.com/todos/1")
-    return response.json()
-    return response.status_code
-    
+class ChatRequest(BaseModel):
+    message: str=Field(min_length=1, max_length=2000)
+
+class ChatResponse(BaseModel):
+    answer:str
+
+@app.post("/chat",response_model=ChatResponse)
+
+async def ask_llm(data:ChatRequest):
+
+    return {
+        "answer": f"You asked: {data.message}"
+    }
+
+@app.post("/chat",response_model=ChatResponse)
+async def chat(data:ChatRequest):
+    try:
+        answer= await ask_llm(data.message)
+        return {
+            "answer": answer
+        }
+    except Exception:
+        raise HTTPException(status_code=500, detail= "AI service unavailable")
+
+
+
+
 	
 
 
