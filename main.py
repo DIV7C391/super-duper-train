166356eb3ec599073e3623e95aa482b1a607bf8e
Divyanshu Dwivedi <divyanshu.d055123@gmail.com>
2026-06-12 13:48:34 +0530

    streaming response add

--- main.py
+++ main.py
@@ -1,41 +1,22 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, Field
-import logging
-import httpx
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+import asyncio
 
-app=FastAPI()
-logging.basicConfig(level=logging.INFO)
-
-class ChatRequest(BaseModel):
-    message: str=Field(min_length=1, max_length=2000)
-
-class ChatResponse(BaseModel):
-    answer:str
-
-@app.post("/chat",response_model=ChatResponse)
-
-async def ask_llm(data:ChatRequest):
-
-    return {
-        "answer": f"You asked: {data.message}"
-    }
-
-@app.post("/chat",response_model=ChatResponse)
-async def chat(data:ChatRequest):
-    try:
-        answer= await ask_llm(data.message)
-        return {
-            "answer": answer
-        }
-    except Exception:
-        raise HTTPException(status_code=500, detail= "AI service unavailable")
+app = FastAPI()
 
 
+async def resp():
+    words = [
+        "RAG ","stands ","for ","Retrieval ","Augmented ","Generation"]
+    for word in words:
 
+        yield word
 
-	
+        await asyncio.sleep(0.5)
 
-
-
-
-
+@app.get("/stream")
+async def response():
+    return StreamingResponse(
+        resp(),
+        media_type="text/plain"
+    )
--- main1.py
+++ main1.py
@@ -1,5 +1,42 @@
  
             
+from fastapi import FastAPI
+from fastapi import HTTPException, UploadFile, File
+from pypdf import PdfReader
+import tempfile
+import logging
+
+app=FastAPI()
+logging.basicConfig(level=logging.INFO)
+
+@app.post("/pdf-info")
+async def info_pdf(file: UploadFile = File(...)):
+    #if file.content_type != "application/pdf":
+        #raise HTTPException(status_code=400, detail="Invalid file type. Only PDF files are allowed.")
+    logging.info("pdf loading")
+    content= await file.read()
+    with tempfile.NamedTemporaryFile(delete=False,suffix=".pdf") as temp_file:
+        temp_file.write(content)
+        
+    try:
+        reader=PdfReader(temp_file.name)
+    except Exception as e:
+        logging.error(f"Error reading PDF: {e}")
+        raise HTTPException(status_code=400, detail="Invalid PDF file.")
     
+    pages=len(reader.pages)
+    char=0
+    text=""
+    for page in reader.pages:
+        char1=page.extract_text()
+        text+=char1
+        char+=len(char1)
     
+    return {
+        "filename": file.filename,
+        "pages": pages,
+        "characters": char,
+        "text": text
+     
+    }
 
